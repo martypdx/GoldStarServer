f97001e2e2be077fa5b8c355daa6d01a36888fbc
Haley Tortorella <hdtort@gmail.com>
2017-09-18 16:39:58 -0700

    Passing search test to find sets

---
+++ test/e2e/search.test.js
@@ -0,0 +1,21 @@
+const db = require('./helpers/db');
+require('dotenv').config();
+const request = require('./helpers/request');
+const { assert } = require('chai');
+
+describe('search route', () => {
+    before(db.drop);
+
+
+    let token = null;
+    before(() => db.getToken().then(t => token = t));
+
+    const query = 'cactus';
+
+    it('conducts a search', () => {
+        return request.get(`/api/search/query=${query}`)
+            .set('Authorization', token)
+            .then(res => res = res.body);
+
+    });
+});
