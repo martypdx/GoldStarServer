330a2daa8d97d37bf04c2202b29c36bf1473701b
Meryl Turner <merylturner08@gmail.com>
2017-09-18 17:00:25 -0700

    bad query test passing

--- test/e2e/search.test.js
+++ test/e2e/search.test.js
@@ -14,9 +14,20 @@ describe('search route', () => {
     it('conducts a search', () => {
         return request.get(`/api/search/query=${query}`)
             .set('Authorization', token)
-            .then(res => res = res.body)
+            .then(res => res.body)
             .then(data => {
                 assert.ok(data[0].title);
+            })
+    });
+
+    const badQuery = ';aksjhdgkashdg';
+
+    it('returns empty with bad query', () => {
+        return request.get(`/api/search/query=${badQuery}`)
+            .set('Authorization', token)
+            .then(res => res.body)
+            .then(data => {
+                assert.equal(data.results, 0);
             })
     });
 });
